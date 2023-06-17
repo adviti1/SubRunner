eeb2215ff323f5c68a2db44f839a5743586cea95
adviti1 <advitigangwar123@gmail.com>
2023-06-17 23:11:00 +0530

    made an idle state

--- Assets/Scripts/CharMovement.cs
+++ Assets/Scripts/CharMovement.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class CharMovement : MonoBehaviour
 {
-    public float Speed;
-    public 
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        transform.Translate(Vector3.up * Speed, Space.World);
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, -5, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            if (Input.GetKeyUp("left"))
+            {
+                transform.Rotate(0, 10, 0);
+            }
+        }
+        if (Input.GetKey("right"))
+        {
+            if (Input.GetKeyUp("right"))
+            {
+                transform.Rotate(0, -10, 0);
+            }
+        }
     }
 }
